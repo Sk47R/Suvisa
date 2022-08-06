fdda6e7a07f7c912d291a5f2c346266c64f05774
sujan <sujankapali50@gmail.com>
2022-08-06 15:35:43 +0545

    edited the hardhat.config.js file :hammer:

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 const fs = require("fs");
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
